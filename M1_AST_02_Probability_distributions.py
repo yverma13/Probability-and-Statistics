1923ff686d35d64b429f2f338bf60203de9d3e4c
Yash-Verma <yash2000@hotmail.co.uk>
2024-10-13 22:50:42 +0100

    13/10/24

---
+++ M1_AST_02_Probability_distributions.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import math
+from scipy.stats import binom, nbinom, bernoulli, poisson, norm, uniform
+from numpy.random import geometric
+sns.set_style('whitegrid')
+import warnings
+warnings.filterwarnings("ignore")
